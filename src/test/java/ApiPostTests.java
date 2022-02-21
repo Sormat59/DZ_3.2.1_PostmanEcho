09ccb7f49fad674b2f66ba01dab24cb1444b5fcf
Sarmat <def2413@bk.ru>
2022-02-21 19:22:24 +0500

    Fix fallen test

--- src/test/java/ApiPostTests.java
+++ src/test/java/ApiPostTests.java
@@ -19,7 +19,7 @@ public class ApiPostTests {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some test data"));
     }
 
 }
